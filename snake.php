b503d68ccf3e14ca6ae2e2d74d333c498a87b36b
BaykovaNadi <BaykovaNadi@yandex.ru>
2019-01-09 21:04:59 +0300

    print string

--- snake.php
+++ snake.php
@@ -1,8 +1,27 @@
 <html>
 <body>
 <?php
-$myvar = "Hello, World";
-echo $myvar;
+$my_array = array(
+    "0" => "- - - - -",
+    "1" => "- - - - -"
+);
+// foreach ($my_array as $my_key => $my_row)
+// {
+//     echo ("$my_row\n");
+// };
+echo ("$my_array[0] \n");
+
+$x1 = 0;
+$y1 = 1;
+$symb1 = "- * - - -";
+$my_array[$x1] = $symb1;
+
+// foreach ($my_array as $my_key => $my_row)
+// {
+//     echo ("$my_row\n");
+// };
+echo ($my_array[0]);
+
 ?>
 </body>
 </html>
