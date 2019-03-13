9769c5e1048894f4e43bc92bc8f3dc63c40ccb5b
BaykovaNadi <BaykovaNadi@yandex.ru>
2019-03-13 16:49:03 +0300

    class Point

--- snake.php
+++ snake.php
@@ -1,23 +1,23 @@
 <?php
+
+include_once "point.php";
+
 $my_array = array(
-    "0" => "- - - - -",
-    "1" => "- - - - -"
+    0 => "- - - - -",
+    1 => "- - - - -",
+    2 => "- - - - -",
+    3 => "- - - - -",
+    4 => "- - - - -",
 );
-// foreach ($my_array as $my_key => $my_row)
-// {
-//     echo ("$my_row\n");
-// };
+
 echo ("$my_array[0] \n");
 
-$x1 = 0;
-$y1 = 1;
-$symb1 = "- * - - -";
-$my_array[$x1] = $symb1;
+$p = new Point();
 
-// foreach ($my_array as $my_key => $my_row)
-// {
-//     echo ("$my_row\n");
-// };
-echo ($my_array[0]);
+$p->$x = 1;
+$p->$y = 1;
+$symb1 = "*";
+$p->$my_array[$x][$y] = $symb1;
+$p->drawPoint();
 
 ?>
