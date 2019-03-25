11e5aeb008b36f2bc1bc4a25843d019baf530754
BaykovaNadi <BaykovaNadi@yandex.ru>
2019-03-25 16:19:02 +0300

    lean object

--- snake.php
+++ snake.php
@@ -1,23 +1,26 @@
 <?php
 
 include_once "point.php";
-
 $my_array = array(
-    0 => "- - - - -",
-    1 => "- - - - -",
-    2 => "- - - - -",
-    3 => "- - - - -",
-    4 => "- - - - -",
+    0 => array (0, 0, 0, 0, 0),
+    1 => array ("-","-","-","-","-"),
+    2 => array ("-","-","-","-","-"),
+    3 => array ("-","-","-","-","-"),
+    4 => array ("-","-","-","-","-"),
 );
-
-echo ("$my_array[0] \n");
 
 $p = new Point();
 
-$p->$x = 1;
-$p->$y = 1;
-$symb1 = "*";
-$p->$my_array[$x][$y] = $symb1;
-$p->drawPoint();
+echo "1: {$my_array[1][2]} \n";
 
+$p->x = 1;
+$p->y = 2;
+$p->sym = "*";
+$my_array[$p->x][$p->y] = $p->sym;
+$p->drawPoint($p->x, $p->y, $my_array);
+echo "3: {$my_array[0]} \n";
+echo "3: {$my_array[1][0]}, {$my_array[1][1]}, {$my_array[1][2]}, {$my_array[1][3]}, {$my_array[1][4]} \n";
+echo "3: {$my_array[2]} \n";
+echo "3: {$my_array[3]} \n";
+echo "3: {$my_array[4]} \n";
 ?>
