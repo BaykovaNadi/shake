f7e597a06000a9724c3723016316b5b5ab4c2eca
BaykovaNadi <BaykovaNadi@yandex.ru>
2019-03-26 16:19:08 +0300

    Success class Point

--- snake.php
+++ snake.php
@@ -1,26 +1,16 @@
 <?php
 
 include_once "point.php";
-$my_array = array(
-    0 => array (0, 0, 0, 0, 0),
-    1 => array ("-","-","-","-","-"),
-    2 => array ("-","-","-","-","-"),
-    3 => array ("-","-","-","-","-"),
-    4 => array ("-","-","-","-","-"),
-);
 
-$p = new Point();
+$p1 = new Point();
+$p1->x = 1;
+$p1->y = 2;
+$p1->sym = "*";
+$p1->drawPoint($p1->x, $p1->y, $p1->sym);
 
-echo "1: {$my_array[1][2]} \n";
-
-$p->x = 1;
-$p->y = 2;
-$p->sym = "*";
-$my_array[$p->x][$p->y] = $p->sym;
-$p->drawPoint($p->x, $p->y, $my_array);
-echo "3: {$my_array[0]} \n";
-echo "3: {$my_array[1][0]}, {$my_array[1][1]}, {$my_array[1][2]}, {$my_array[1][3]}, {$my_array[1][4]} \n";
-echo "3: {$my_array[2]} \n";
-echo "3: {$my_array[3]} \n";
-echo "3: {$my_array[4]} \n";
+$p2 = new Point();
+$p2->x = 3;
+$p2->y = 4;
+$p2->sym = "#";
+$p2->drawPoint($p2->x, $p12>y, $p2->sym);
 ?>
